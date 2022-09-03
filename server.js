e9f100f097775a8d368ddf9cf10dd01c0c3a1aa5
Amaresh Barik <96060638+amareshcoding@users.noreply.github.com>
2022-09-03 20:38:59 +0530

    socket io connected

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const ACTIONS = require('./src/actions');
 //
 const app = express();
 
@@ -8,8 +9,45 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
+const userSocketMap = {};
+function getAllConnectClients(roomId) {
+  return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+    (socketId) => {
+      return {
+        socketId,
+        username: userSocketMap[socketId],
+      };
+    }
+  );
+}
+
 io.on('connection', (socket) => {
   console.log(`socket connected ${socket.id}`);
+
+  socket.on(ACTIONS.JOIN, ({ roomId, username }) => {
+    userSocketMap[socket.id] = username;
+    socket.join(roomId);
+    const clients = getAllConnectClients(roomId);
+    clients.forEach(({ socketId }) => {
+      io.to(socketId).emit(ACTIONS.JOINED, {
+        clients,
+        username,
+        socketId: socket.id,
+      });
+    });
+  });
+
+  socket.on('disconnecting', () => {
+    const rooms = [...socket.rooms];
+    rooms.forEach((roomId) => {
+      socket.in(roomId).emit(ACTIONS.DISCONNECTED, {
+        socketId: socket.id,
+        username: userSocketMap[socket.id],
+      });
+    });
+    delete userSocketMap[socket.id];
+    socket.leave();
+  });
 });
 
 //
--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -3,25 +3,70 @@ import ACTIONS from '../actions';
 import Editor from '../components/Editor';
 import User from '../components/User';
 import { initSocket } from '../socket';
-import { useLocation } from 'react-router-dom';
+import {
+  useLocation,
+  useParams,
+  useNavigate,
+  Navigate,
+} from 'react-router-dom';
+import toast from 'react-hot-toast';
 
 const EditorPage = () => {
   const socketRef = useRef(null);
   const location = useLocation();
+  const { roomId } = useParams();
+  const reactNavigator = useNavigate();
+  const [clients, setClints] = useState([]);
+  //
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
-      // socketRef.current.emit(ACTIONS.JOIN, {
-      //   roomId,
-      //   username:location.state?.username
-      // });
+
+      socketRef.current.on('connect_error', (err) => handleErrors(err));
+      socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log('socket error', e);
+        toast.error('Socket connected railed, try again later.');
+        reactNavigator('/');
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      //Listening for joined client
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClints(clients);
+        }
+      );
+      //Disconnected Listening
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        //
+        toast.success(`${username} left the room.`);
+        setClints((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
     };
     init();
+    return () => {
+      socketRef.current.disconnect();
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+    };
   }, []);
-  const [users, setUsers] = useState([
-    { socketId: 1, username: 'Amaresh' },
-    { socketId: 2, username: 'Barik' },
-  ]);
+
+  if (!location.state) {
+    return <Navigate to={'/'} />;
+  }
   return (
     <div className="editorContainer">
       <div className="editorLeftBox">
@@ -35,8 +80,8 @@ const EditorPage = () => {
           </div>
           <h3>Connected</h3>
           <div className="userList">
-            {users.map((user) => (
-              <User username={user.username} key={user.socketId} />
+            {clients.map((client) => (
+              <User username={client.username} key={client.socketId} />
             ))}
           </div>
         </div>
